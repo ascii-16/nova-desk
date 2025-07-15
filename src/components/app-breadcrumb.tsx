f0230f6a97267dc951bae33d1ff3fb5ddcc6279d
ascii-16 <mail.akhil99@gmail.com>
2025-07-15 21:08:53 +0530

    feat: capitalise breadcrumb

--- src/components/app-breadcrumb.tsx
+++ src/components/app-breadcrumb.tsx
@@ -24,7 +24,7 @@ export function AppBreadCrumb() {
 
   return (
     <Breadcrumb>
-      <BreadcrumbList>
+      <BreadcrumbList className="capitalize">
         {pathArray.map((item, i) => (
           <Fragment key={item.href}>
             <BreadcrumbItem>
